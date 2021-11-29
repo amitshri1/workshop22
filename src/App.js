5fbced3e9b9fc966c3b83681caf20bba3468ed48
Amit Shrivastava <amits@leewayhertz.com>
2021-11-29 19:04:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Login from './features/Login';
+import  { useSelector} from "react-redux";
+import { selectUser } from './Reducer/userReducer.js';
+import Logout from './features/Logout';
 function App() {
+  const user=useSelector(selectUser);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+   <>
+   {user?<Logout/>:<Login/>}
+   </>
   );
 }
 
